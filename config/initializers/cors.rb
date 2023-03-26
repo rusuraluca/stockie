be31b97f193ea652d8d1c9729d421e22283b0100
Raluca Rusu <rr.ralucarusu@gmail.com>
2023-03-27 02:52:34 +0300

    Lab 4: 4.1 Added tests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "0.0.0.0:3000"
+    origins "ec2-13-48-129-48.eu-north-1.compute.amazonaws.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
    end
 end
