ca463f5cf9262d143be91618ed944105afdbbffd
Raluca Rusu <rr.ralucarusu@gmail.com>
2023-03-27 03:39:13 +0300

    Lab 4: 4.2 Added CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,5 +16,5 @@ Rails.application.config.hosts = [
     IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
     IPAddr.new("::/0"),             # All IPv6 addresses.
     "localhost",                    # The localhost reserved domain.
-    ENV["ec2-13-48-129-48.eu-north-1.compute.amazonaws.com"]  # Additional comma-separated hosts for development.
+    ENV["ec2-13-50-226-35.eu-north-1.compute.amazonaws.com"]  # Additional comma-separated hosts for development.
 ]
