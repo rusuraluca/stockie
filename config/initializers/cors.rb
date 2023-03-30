9152ca8b740dc6b86144c0881aa28b99db0a713e
Raluca Rusu <rr.ralucarusu@gmail.com>
2023-03-30 12:14:36 +0300

    Lab 4: 4.2 Deploy to a virtual machine

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,10 +11,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
    end
 end
-
-Rails.application.config.hosts = [
-    IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
-    IPAddr.new("::/0"),             # All IPv6 addresses.
-    "localhost",                    # The localhost reserved domain.
-    ENV["ec2-13-50-101-86.eu-north-1.compute.amazonaws.com"]  # Additional comma-separated hosts for development.
-]
