2a139aab910f285f17b06b9ad94a91519e1149aa
Raluca Rusu <rr.ralucarusu@gmail.com>
2023-04-02 15:35:12 +0300

    Lab 5: 5.6 Netlify Deployment

--- stockie-front-end/src/constants.ts
+++ stockie-front-end/src/constants.ts
@@ -1,5 +1,5 @@
 const PROD_BACKEND_API_URL = "/api";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:3000/api";
 
-export const BACKEND_API_URL = 
+export const BACKEND_API_URL =
     process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
