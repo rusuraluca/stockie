fa087d1f110df3062a756d65cec62ff98c02ded8
Raluca Rusu <rr.ralucarusu@gmail.com>
2023-05-21 13:05:53 +0300

    Task 11.2 Implement a chat page

--- stockie-back-end/config/environments/test.rb
+++ stockie-back-end/config/environments/test.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end
