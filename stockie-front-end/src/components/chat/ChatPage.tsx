bc7531ad0c44b5cc8378fd725cd829eb62c3a15f
Raluca Rusu <rr.ralucarusu@gmail.com>
2023-05-21 13:01:04 +0300

    Task 11.2 Implement a chat page

--- stockie-front-end/src/components/chat/ChatPage.tsx
+++ stockie-front-end/src/components/chat/ChatPage.tsx
@@ -5,7 +5,7 @@ import ActionCable from 'actioncable';
 import { Box, Container, TextField, Button } from '@mui/material';
 import {Formik, Form, FormikHelpers, Field, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
-import { BACKEND_URL } from '../../constants';
+import { BACKEND_API_URL } from '../../constants';
 
 interface Message {
     sender: string;
@@ -37,7 +37,7 @@ const Chat: React.FC = () => {
     };
 
     useEffect(() => {
-        const cable = ActionCable.createConsumer(`${BACKEND_URL}/cable`);
+        const cable = ActionCable.createConsumer(`${BACKEND_API_URL}/cable`);
         const sub = cable.subscriptions.create(
             {
                 channel: 'ChatChannel',
@@ -70,7 +70,7 @@ const Chat: React.FC = () => {
 
     return (
         <Container>
-            <ActionCableProvider url={`${BACKEND_URL}/cable`}>
+            <ActionCableProvider url={`${BACKEND_API_URL}/cable`}>
                 <h1 style={{ margin: '24px 0' }}>Chat</h1>
                 <Box>
                     <Formik
